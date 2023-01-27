c14d417e32afa49509886a14375fd4b3aea6e4f6
Hitallo <hitallomoreira13@gmail.com>
2023-01-27 14:34:18 -0300

    feat: finish footer

--- src/components/Footer/Footer.vue
+++ src/components/Footer/Footer.vue
@@ -1,7 +1,11 @@
 <template>
     <footer class="attribution">
-        Challenge by<a href="https://www.frontendmentor.io?ref=challenge" target="_blank"> Frontend Mentor</a>
-        Coded by<a href="https://github.com/Hitallo-moreira?tab=repositories" target="_blank"> Hitallo Feitosa</a>
+        <span>
+            Challenge by<a href="https://www.frontendmentor.io?ref=challenge" target="_blank"> Frontend Mentor</a>
+        </span>
+        <span class="codeby">
+            Coded by<a href="https://github.com/Hitallo-moreira?tab=repositories" target="_blank"> Hitallo Feitosa</a>
+        </span>
     </footer>
 </template>
 <script>
